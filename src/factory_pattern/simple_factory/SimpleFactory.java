68368b7b67b57fcb4af199d8d8b437c61dec28e7
2214576751@qq.com <hxl19920401>
2018-12-09 10:30:19 +0800

    简单工厂模式

--- src/factory_pattern/simple_factory/SimpleFactory.java
+++ src/factory_pattern/simple_factory/SimpleFactory.java
@@ -3,11 +3,19 @@ package factory_pattern.simple_factory;
 
 /**
  * 关键点：产品和工厂
+ * 定制抽象产品接口
+ * 定制具体产品实现类
+ * 定制工厂类
+ *
+ * 不过，实际应用中，只有很简单的情况下才会使用这个，但是，很简单的情况下，也不太需要这种，所以，这个只是作为了解，实际并没有什么卵用
+ * 实际开发应用中会用的是工厂模式和抽象工厂
  */
 public class SimpleFactory {
 
     public static Car creatCar(String level) {
         Car car = null;
+
+        //下面也可以用swich case
         if (level.equals(Car.UP_CAR)) {
             car = new UpCar();
         } else if (level.equals(Car.MID_CAR)) {
